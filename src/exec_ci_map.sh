491a07e5061c84f173472d3ef9811d7660930e3e
kohei <matsumoto.k.bs@m.titech.ac.jp>
2023-11-10 19:16:49 +0900

    とりあえずいらないものを削除

--- src/exec_ci_map.sh
+++
@@ -1,5 +0,0 @@
-pwd
-cd work/Main
-. ~/anaconda3/etc/profile.d/conda.sh
-conda activate habitat
-python ShowingGraph/ShowingCIMap.py 
--- src/exec_test.sh
+++
@@ -1,6 +0,0 @@
-pwd
-cd work/Main
-. ~/anaconda3/etc/profile.d/conda.sh
-conda activate habitat
-#python make_dataset.py
-python make_ci_map.py
