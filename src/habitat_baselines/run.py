beb07971391a818f3f21c9bb7ad519d6aadc6aa5
kohei <matsumoto.k.bs@m.titech.ac.jp>
2023-11-13 19:11:24 +0900

    マップ作成完了

--- src/habitat_baselines/run.py
+++ src/habitat_baselines/run.py
@@ -17,130 +17,24 @@ from habitat_baselines.config.default import get_config
 from utils.log_manager import LogManager
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--run-type",
-        choices=["train", "eval"],
-        required=True,
-        help="run type of the experiment (train or eval)",
-    )
-    parser.add_argument(
-        "--exp-config",
-        type=str,
-        required=True,
-        help="path to config yaml containing info about experiment",
-    )
-
-    parser.add_argument(
-        "--agent-type",
-        choices=["no-map", "oracle", "oracle-ego", "proj-neural", "obj-recog"],
-        required=True,
-        help="agent type: oracle, oracleego, projneural, objrecog",
-    )
-
-    parser.add_argument(
-        "opts",
-        default=None,
-        nargs=argparse.REMAINDER,
-        help="Modify config options from command line",
-    )
-
-    args = parser.parse_args()
-    run_exp(**vars(args))
-
-
-def run_exp(exp_config: str, run_type: str, agent_type: str, opts=None) -> None:
-    r"""Runs experiment given mode and config
-
-    Args:
-        exp_config: path to config file.
-        run_type: "train" or "eval.
-        opts: list of strings of additional config options.
-
-    Returns:
-        None.
-    """
-    config = get_config(exp_config, opts)
-    random.seed(config.TASK_CONFIG.SEED)
-    np.random.seed(config.TASK_CONFIG.SEED)
-
-    config.defrost()
-    config.TRAINER_NAME = agent_type
-    config.TASK_CONFIG.TRAINER_NAME = agent_type
-    config.freeze()
-
-    if agent_type in ["oracle", "oracle-ego", "no-map"]:
-        trainer_init = baseline_registry.get_trainer("oracle")
-        config.defrost()
-        config.RL.PPO.hidden_size = 512 if agent_type=="no-map" else 768
-        config.TASK_CONFIG.SIMULATOR.DEPTH_SENSOR.MIN_DEPTH = 0.5
-        config.TASK_CONFIG.SIMULATOR.DEPTH_SENSOR.MAX_DEPTH = 5.0
-        config.TASK_CONFIG.SIMULATOR.AGENT_0.HEIGHT = 1.5
-        if agent_type == "oracle-ego":
-            config.TASK_CONFIG.TASK.MEASUREMENTS.append('FOW_MAP')
-        config.freeze()
-    else:
-        trainer_init = baseline_registry.get_trainer("non-oracle")
-        config.defrost()
-        config.RL.PPO.hidden_size = 512
-        config.freeze()
-        
-    assert trainer_init is not None, f"{config.TRAINER_NAME} is not supported"
-    
-    #ログファイルの設定   
-    start_date = datetime.datetime.now().strftime('%y-%m-%d %H-%M-%S') 
-    log_manager_train = LogManager()
-    log_manager_train.setLogDirectory("./log/" + start_date + "/train")
-    log_manager_val = LogManager()
-    log_manager_val.setLogDirectory("./log/" + start_date + "/val")
-    
-    
-    device = (
-        torch.device("cuda", config.TORCH_GPU_ID)
-        if torch.cuda.is_available()
-        else torch.device("cpu")
-    )
-    print("-----------------------------------")
-    print("device:" + str(device))
-    print("-----------------------------------")
-    
-    trainer = trainer_init(config)
-
-    if run_type == "train":
-        trainer.train(log_manager_train, start_date)
-    elif run_type == "eval":
-        trainer.eval(log_manager_val, start_date)
-        
-    end_date = datetime.datetime.now().strftime('%y-%m-%d %H-%M-%S')
-    print("Start at " + start_date)
-    print("End at " + end_date)
-    
-
-def test():
     exp_config = "habitat_baselines/config/maximuminfo/ppo_maximuminfo.yaml"
     agent_type = "oracle-ego"
-    run_type = "train"
-    #run_type = "eval"
+    run_type = "eval"
     start_date = datetime.datetime.now().strftime('%y-%m-%d %H-%M-%S') 
     
     if run_type == "eval":
         datadate = "23-10-26 18-29-56"
-    else:
-       datadate = "" 
-    
+
     config = get_config(exp_config)
-    
     
     random.seed(config.TASK_CONFIG.SEED)
     np.random.seed(config.TASK_CONFIG.SEED)
     
     config.defrost()
-    config.TASK_CONFIG.DATASET.DATA_PATH = "data/datasets/multinav/3_ON/{split}/{split}.json.gz"
     config.TRAINER_NAME = agent_type
     config.TASK_CONFIG.TRAINER_NAME = agent_type
     config.CHECKPOINT_FOLDER = "cpt/" + start_date
     config.EVAL_CKPT_PATH_DIR = "cpt/" + datadate 
-    #config.TEST_EPISODE_COUNT = 1000
     #config.VIDEO_OPTION = []
     config.VIDEO_OPTION = ["disk"]
     config.freeze()
@@ -186,15 +80,11 @@ def test():
             
         trainer.train(log_manager, start_date)
     elif run_type == "eval":
-        trainer.eval(log_manager, start_date)
+        trainer._exec_kachaka(log_manager, start_date)
        
     end_date = datetime.datetime.now().strftime('%y-%m-%d %H-%M-%S') 
     print("Start at " + start_date)
     print("End at " + end_date)
 
 if __name__ == "__main__":
-    #main()
-    test()
-
-    #MIN_DEPTH: 0.5
-    #MAX_DEPTH: 5.0
+    main()
---
+++ test/map_test.py
@@ -0,0 +1,199 @@
+#from IPython.display import Image, display
+from PIL import Image
+import sys
+import os
+import io
+
+import numpy as np
+
+import kachaka_api
+sys.path.append(f"/Users/{os.environ['USER']}/Desktop/habitat2kachaka/kachaka-api/python/")
+
+
+class LogWriter:
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+        
+        #ファイルを新規作成 or 上書き
+        with open(self.file_path, "w") as f:
+            pass
+            
+    #改行なし
+    def write(self, log: str) -> None:
+        #ファイルへ追記
+        with open(self.file_path, 'a', newline='') as f:
+            f.write(log + ",")
+            
+    #改行あり
+    def writeLine(self, log: str="") -> None:
+        #ファイルへ追記
+        with open(self.file_path, "a") as f:
+            f.write(log + "\n")
+            
+            
+class LogManager:
+    def __init__(self) -> None:
+        self.writers = {}
+    
+    def setLogDirectory(self, dir_path: str) -> None:
+        self.dir_path = dir_path
+        if not os.path.exists(self.dir_path):
+            # ディレクトリが存在しない場合、ディレクトリを作成する
+            os.makedirs(self.dir_path)
+            
+        self.dir_path = "./" + dir_path + "/"
+        
+    def makeDir(self, path_dir: str) -> str:
+        path_dir = self.dir_path + path_dir
+        if not os.path.exists(path_dir):
+            # ディレクトリが存在しない場合、ディレクトリを作成する
+            os.makedirs(path_dir)
+            
+        return path_dir
+    
+    def createLogWriter(self, key: str) -> LogWriter:
+        if key in self.writers:
+            return self.writers[key]
+        
+        writer = LogWriter(self.dir_path + key + ".csv")
+        self.writers[key] = writer
+        return writer
+    
+    #テスト用
+    def printWriters(self) -> None:
+        print(self.writers)
+        
+        
+def resize_map(map):
+    h = len(map)
+    w = len(map[0])
+    size = 3
+    
+    resized_map = np.zeros((int(h/size), int(w/size)))
+    print("h=" + str(h) + ", w=" + str(w) + ", h'=" + str(len(resized_map)) + ", w'=" + str(len(resized_map[0])))
+
+    # mapのresize
+    for i in range(len(resized_map)):
+        for j in range(len(resized_map[0])):
+            flag = False
+            num_0 = 0
+            num_2 = 0
+            for k in range(size):
+                if flag == True:
+                    break
+                
+                if size*i+k >= h:
+                    break
+                
+                for l in range(size):
+                    if size*j+l >= w:
+                        break
+                    if map[size*i+k][size*j+l] == 1:
+                        resized_map[i][j] = 1
+                        flag = True
+                    elif map[size*i+k][size*j+l] == 0:
+                        num_0 += 1
+                    elif map[size*i+k][size*j+l] == 2:
+                        num_2 += 1
+                        
+            if flag == False:
+                if num_0 > num_2:
+                    resized_map[i][j] = 0
+                else:
+                    resized_map[i][j] = 2
+            
+    # borderをちゃんと作る
+    for i in range(len(resized_map)):
+        for j in range(len(resized_map[0])):
+            flag = False
+            if resized_map[i][j] == 2:
+                for k in [-1, 1]:
+                    if flag == True:
+                        break
+                    if i+k < 0 or i+k >= len(resized_map):
+                        continue
+                    for l in [-1, 1]:
+                        if j+l < 0 or j+l >= len(resized_map[0]):
+                            continue
+                        if resized_map[i+k][j+l] == 0:
+                            resized_map[i][j] = 1
+                            flag = True
+                            break
+    
+    return resized_map
+
+def clip_map(map):
+    grid_delta = 3
+    range_x = np.where(np.any(map != 0, axis=1))[0]
+    range_y = np.where(np.any(map != 0, axis=0))[0]
+    
+    ind_x_min = range_x[0]
+    ind_x_max = range_x[-1]
+    ind_y_min = range_y[0]
+    ind_y_max = range_y[-1]
+
+    return map[
+            ind_x_min - grid_delta : ind_x_max + grid_delta,
+            ind_y_min - grid_delta : ind_y_max + grid_delta,
+        ]
+        
+        
+def create_map(map_data):
+    h = len(map_data)
+    w = len(map_data[0])
+    map = np.zeros((h, w))
+    
+    for i in range(h):
+        for j in range(w):
+            # 不可侵領域
+            if map_data[i][j][0] == 244:
+                map[i][j] = 0
+            elif map_data[i][j][0] == 191:
+                map[i][j] = 1
+            elif map_data[i][j][0] == 253:
+                map[i][j] = 2
+            else:
+                map[i][j] = -1
+                
+    return map
+                
+
+if __name__ == "__main__":
+    client = kachaka_api.KachakaApiClient(sys.argv[1])
+    client.update_resolver()
+    
+    logmanager = LogManager()
+    logmanager.setLogDirectory("./")
+    writer = logmanager.createLogWriter("map")
+    
+    map = client.get_png_map()
+    print(map.name)
+    print(map.resolution, map.width, map.height)
+    print(map.origin)
+
+    map_img = Image.open(io.BytesIO(map.data))
+    
+    data = map_img.getdata()
+    map_data = np.array(data) 
+    
+    # 分割サイズ
+    chunk_size = map.width
+    # 分割
+    map_data = np.array(np.array_split(map_data, range(chunk_size, len(map_data), chunk_size), axis=0))
+    
+    print("map_data: " + str(len(map_data)) + "," + str(len(map_data[0])))
+    map_data = create_map(map_data)
+    map_data = resize_map(map_data)
+    print("resize: " + str(len(map_data)) + "," + str(len(map_data[0])))
+    clipped_map = clip_map(map_data)
+    #clipped_map = map_data
+    print("clip: " + str(len(clipped_map)) + "," + str(len(clipped_map[0])))
+
+    for i in range(len(clipped_map)):
+        for j in range(len(clipped_map[0])):
+            writer.write(str(clipped_map[i][j]))
+        writer.writeLine()
+    
+    map_img.save("test.png")
+    
+    
